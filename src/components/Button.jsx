c7b0f55e507dd42ebb1fc8ae7f44ce60aec3eaf2
mariokarajallo <mariokarajallo92@gmail.com>
2023-07-03 14:07:36 -0400

    feat: create button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+function Button() {
+  return (
+    <button
+      type="button"
+      className="h-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:ring-offset-2 hover:ring-lime-500"
+    >
+      -
+    </button>
+  );
+}
+export default Button;
