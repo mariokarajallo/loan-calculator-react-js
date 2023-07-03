f0ede15715f0cf51af2edf695bdddd6f5e2e7f0c
mariokarajallo <mariokarajallo92@gmail.com>
2023-07-03 14:45:01 -0400

    feat: add setState -> select

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { formatearDinero } from "./helpers";
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+
   const min = 0;
   const max = 20000;
   const step = 100;
@@ -48,7 +50,11 @@ function App() {
         {formatearDinero(cantidad)}
       </p>
 
-      <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500">
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={meses}
+        onChange={(e) => setMeses(+e.target.value)}
+      >
         <option value="6">6 meses</option>
         <option value="12">12 meses</option>
         <option value="24">24 meses</option>
